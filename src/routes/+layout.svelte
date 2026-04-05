d4df2dfada840a87e86f51393bcf7c0e6484d8d8
Minhyeok Park <pmh_only@pmh.codes>
2026-04-05 15:20:41 +0900

    feat: add root navigation

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -20,7 +20,9 @@
 
 <div class="flex h-full max-h-270 w-full max-w-xl flex-col px-4 py-2">
   <nav class="text-center">
-    txt<a href={resolve(`/auth?state=${state}`)}>.</a>
+    <a href={resolve('/')}>txt</a><a
+      href={resolve(`/auth?state=${state}`)}>.</a
+    >
   </nav>
 
   <div class="cont flex grow flex-col">
